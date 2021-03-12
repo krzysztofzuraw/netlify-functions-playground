573732273fb1ecafec03bdec62d67667e7a042a3
Krzysztof Żuraw <services@kzuraw.com>
2021-03-12 19:52:28 +0100

    feat: test again

--- functions/social-card/social-card.js
+++ functions/social-card/social-card.js
@@ -1,5 +1,6 @@
 const playwright = require("playwright-aws-lambda");
 const nunjucks = require("nunjucks");
+const path = require("path");
 
 exports.handler = async function (event, ctx) {
   // const browser = await playwright.launchChromium();
@@ -8,12 +9,12 @@ exports.handler = async function (event, ctx) {
   //   width: 1200,
   //   height: 630,
   // });
-  nunjucks.configure(".", { autoescape: true });
+  nunjucks.configure(path.resolve(__dirname), { autoescape: true });
   const html = nunjucks.render("index.html", {
     title:
       "How to dispatch multiple actions based on api call in redux-observable",
   });
-  console.log(html);
+  console.log(path.resolve(__dirname));
   // await page.setContent(html);
   // const screenshotBuffer = await page.screenshot();
   // await browser.close();
