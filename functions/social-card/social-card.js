8d7702d6c6533fff9d77a25e240e97cda9d7bec6
Krzysztof Żuraw <services@kzuraw.com>
2021-03-12 19:47:10 +0100

    feat: add test

--- functions/social-card/social-card.js
+++ functions/social-card/social-card.js
@@ -1,26 +1,30 @@
 const playwright = require("playwright-aws-lambda");
 const nunjucks = require("nunjucks");
 
-nunjucks.configure(".", { autoescape: true });
-
 exports.handler = async function (event, ctx) {
-  const browser = await playwright.launchChromium();
-  const page = await browser.newPage();
-  page.setViewportSize({
-    width: 1200,
-    height: 630,
+  // const browser = await playwright.launchChromium();
+  // const page = await browser.newPage();
+  // page.setViewportSize({
+  //   width: 1200,
+  //   height: 630,
+  // });
+  nunjucks.configure(".", { autoescape: true });
+  const html = nunjucks.render("index.html", {
+    title:
+      "How to dispatch multiple actions based on api call in redux-observable",
   });
-  const html = nunjucks.render("index.html", { title: "bar" });
-  await page.setContent(html);
-  const screenshotBuffer = await page.screenshot();
-  await browser.close();
+  console.log(html);
+  // await page.setContent(html);
+  // const screenshotBuffer = await page.screenshot();
+  // await browser.close();
   return {
-    isBase64Encoded: true,
+    // isBase64Encoded: true,
     statusCode: 200,
-    headers: {
-      "Content-Type": "image/png",
-      "Content-Length": screenshotBuffer.length.toString(),
-    },
-    body: screenshotBuffer.toString("base64"),
+    body: html,
+    // headers: {
+    //   "Content-Type": "image/png",
+    //   "Content-Length": screenshotBuffer.length.toString(),
+    // },
+    // body: screenshotBuffer.toString("base64"),
   };
 };
